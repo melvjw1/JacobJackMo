fef62c37d10b82c6781ca367b13c6ddcd1f06adf
Jack Gundry (1000133609) <GUNDJP1@student.op.ac.nz>
2024-09-26 15:33:49 +1200

    Skeleton

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectStudio
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void Jack()
+        {
+
+        }
+        static void Mohammad()
+        {
+
+        }
+        static void Jacob()
+        {
+
+        }
+    }
+}
