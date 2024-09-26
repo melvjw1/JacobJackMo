122824385bb0534aad224f97d05731fdbf6e2be5
Jacob Melvin (1000019523) <MELVJW1@student.op.ac.nz>
2024-09-26 15:58:51 +1200

    Stuff

--- Program.cs
+++ Program.cs
@@ -18,7 +18,36 @@
         }
         static void Jacob()
         {
-            Console.WriteLine("This is Jacob");
+            Random rand = new Random();
+            int[] lotto = new int[20];
+            int temp, count = 0;
+
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                temp = rand.Next(lotto.Length);
+                while (count < i)
+                {
+
+                    if (lotto[count] == temp)
+                    {
+                        temp = rand.Next(lotto.Length);
+                        count = 0;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+
+                }
+                count = 0;
+                lotto[i] = temp;
+
+            }
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                Console.WriteLine(lotto[i]);
+            }
+            Console.ReadLine();
 
         }
     }
